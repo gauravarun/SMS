bfa0d3a6019ac2cdec6a9d02e4abddc55059fc84
Gaurav Arun <84666483+gauravarun@users.noreply.github.com>
2022-12-06 12:47:10 +0530

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,14 @@
+
+from twilio.rest import Client
+
+account_sid = 'AC7c011252b7c1d265887b003270663deb'
+auth_token = 'a1eb2a039fcd54d1f3f37ddf9ecad0e4'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Hi there',
+                              from_='+15017122661',
+                              to='+916361448206'
+                          )
+
+print(message.sid)
